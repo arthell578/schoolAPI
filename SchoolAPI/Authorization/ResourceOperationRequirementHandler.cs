93594f9644b8d89d38207af08b338bf8c311df20
Marcin <s18676@pjwstk.edu.pl>
2022-11-03 15:44:03 +0100

    read, create allowed for every user

--- SchoolAPI/Authorization/ResourceOperationRequirementHandler.cs
+++ SchoolAPI/Authorization/ResourceOperationRequirementHandler.cs
@@ -5,9 +5,14 @@ namespace SchoolAPI.Authorization
 {
     public class ResourceOperationRequirementHandler : AuthorizationHandler<ResourceOperationRequirement, School>
     {
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, School resource)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, 
+            ResourceOperationRequirement requirement, School school)
         {
-            throw new NotImplementedException();
+            if(requirement.Operation == ResourceOperation.Read ||
+                requirement.Operation == ResourceOperation.Create)
+            {
+                context.Succeed(requirement);
+            }
         }
     }
 }
