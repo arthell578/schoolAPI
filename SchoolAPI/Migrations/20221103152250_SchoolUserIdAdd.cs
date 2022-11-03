b1137caf52999d5ce4ac190ca9aa2132b7185b20
Marcin <s18676@pjwstk.edu.pl>
2022-11-03 16:23:06 +0100

    addUserId migration applied

---
+++ SchoolAPI/Migrations/20221103152250_SchoolUserIdAdd.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAPI.Migrations
+{
+    public partial class SchoolUserIdAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Schools",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schools_CreatedById",
+                table: "Schools",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schools_Teachers_CreatedById",
+                table: "Schools",
+                column: "CreatedById",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schools_Teachers_CreatedById",
+                table: "Schools");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schools_CreatedById",
+                table: "Schools");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Schools");
+        }
+    }
+}
