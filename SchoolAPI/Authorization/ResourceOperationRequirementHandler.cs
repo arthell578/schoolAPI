5dcb4922579cb4e98bc3ef68aaf881c4bd3a67a7
Marcin <s18676@pjwstk.edu.pl>
2022-11-03 15:40:48 +0100

    resource auth handler created

---
+++ SchoolAPI/Authorization/ResourceOperationRequirementHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using SchoolAPI.Entities;
+
+namespace SchoolAPI.Authorization
+{
+    public class ResourceOperationRequirementHandler : AuthorizationHandler<ResourceOperationRequirement, School>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, School resource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
