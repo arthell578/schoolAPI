ecf257ff551f87c6c1ea24221920ac32fe2e23b3
Marcin <s18676@pjwstk.edu.pl>
2022-11-08 21:29:19 +0100

    props added

--- SchoolAPI/Models/RestaurantQuery.cs
+++ SchoolAPI/Models/RestaurantQuery.cs
@@ -3,6 +3,9 @@
     public class RestaurantQuery
     {
 
+        public string SearchPhrase { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
 
 
     }
